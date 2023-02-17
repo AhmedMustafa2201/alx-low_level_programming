b1da16d35af0736ca2221cc6408e482c222365c0
Your Name <you@example.com>
2023-02-17 07:46:47 -0800

    6,7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
