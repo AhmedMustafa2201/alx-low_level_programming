a9e9f463587f0b0d1c7945ed08adef1eeec35e18
Your Name <you@example.com>
2023-02-17 09:25:25 -0800

    100,101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l, n, m;
+
+	for (l = 0; l <= 7; l++)
+	{
+		for (n = 1; n <= 8; n++)
+		{
+			for (m = 2; m <= 9; m++)
+			{
+				if (l > n && n > m)
+				{
+					putchar(l + '0');
+					putchar(n + '0');
+					putchar(m + '0');
+					if (l != 7 || n != 8 || m != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
