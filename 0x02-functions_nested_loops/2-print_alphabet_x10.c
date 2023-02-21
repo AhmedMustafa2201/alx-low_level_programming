c604c01abe92f0ae7136e12d5f4848c9e32124a1
Your Name <my@example.com>
2023-02-21 12:41:05 -0800

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - prints print_alphabet_x10
+ *
+ * Return nothing
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
