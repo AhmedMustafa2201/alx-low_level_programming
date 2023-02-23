3691596915bdb1b62d636f984fe488921a714520
Your Name <my@example.com>
2023-02-23 13:52:54 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
