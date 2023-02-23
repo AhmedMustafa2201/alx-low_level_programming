6799742f06b4f750db74d57885b641fd458c31d0
Your Name <my@example.com>
2023-02-23 14:37:49 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+# include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		int count = 1;
+
+		while (count < 15)
+		{
+			if (count >= 10)
+			{
+				_putchar((count / 10) + '0');
+				_putchar((count % 10) + '0');
+			}
+			else
+			{
+				_putchar(count + '0');
+			}
+			count++;
+		}
+		_putchar('\n');
+		n++;
+	}
+	_putchar('\n');
+}
