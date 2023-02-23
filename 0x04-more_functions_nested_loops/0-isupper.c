d66ed3009db820ba96f92878aa8a4245170af35a
Your Name <my@example.com>
2023-02-23 13:32:21 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+# include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: the ascii char nymber
+ * Return: int
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
