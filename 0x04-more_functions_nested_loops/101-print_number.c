af976326e1d058fac3ab6fb7ce34d0c7fdb6aed4
Your Name <my@example.com>
2023-02-24 01:30:25 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,18 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n *= -1;
 		_putchar('-');
-	} 
-	else
-	{
-		n1 = n;
 	}
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+	k /= 10;
 
-	_putchar((n1 % 10) + '0');
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
 }
