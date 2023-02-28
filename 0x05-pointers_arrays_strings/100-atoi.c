c13703bc838f125b56c7effce504cc31a7bb6d31
ahmed_mustafa <ahmed_mustafa@mail.com>
2023-02-27 23:58:31 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,10 +16,10 @@ int _atoi(char *s)
 
 	while (*s++)
 	{
-		if (*s >= '0' && *s <= '9')
-			result = (result * 10) + (*s - '0');
-		else if (*s == '-')
+		if (*s == '-')
 			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
 		else if (result > 0)
 			break;
 	}
