f59f672e0fc1243203b0c19cff009af720523ca4
ahmed_mustafa <ahmed_mustafa@mail.com>
2023-03-06 13:21:28 -0800

    update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
